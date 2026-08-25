883b3b2e855d62e28f9326755cd9aa9117fd291d
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-25 23:19:25 +0000

    atualizando

--- 04-exemplo.js
+++ 04-exemplo.js
@@ -1,11 +1,11 @@
-let produto = 'arroz';
+let produto = 'feijão';
 
 switch(produto) {
   case 'arroz':
     console.log('Produto: arroz. Preço: R$ 5,00');
     break;
-  case 'feijao':
-    console.log('Produto: feijao. Preço: R$ 8,00');
+  case 'feijão':
+    console.log('Produto: feijão. Preço: R$ 8,00');
     break;
   default:
     console.log('Produto não encontrado');
