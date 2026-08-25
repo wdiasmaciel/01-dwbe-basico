724c63ca23b74c6dda851b0d3ebeb3f4ee10f49c
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-25 16:07:52 +0000

    Exemplo

--- exemplo8.js
+++ exemplo8.js
@@ -1,4 +1,31 @@
 let vet = ['casa', "carro", true, 6.7, [1, 2, 3]]
 
+console.log("1) Elementos do vetor:");
 for (let i = 0; i < vet.length; i++) 
   console.log(`vet[${i}] = ${vet[i]}`);
+
+console.log("\n2) Elementos do vetor:");
+for (let elemento in vet)
+  console.log(`vet[${elemento}] = ${vet[elemento]}`);
+
+console.log("\n3) Elementos do vetor:");
+let resultadoForEach = vet.forEach(function(elemento, indice) {
+  console.log(`vet[${indice}] = ${elemento}`);
+  return elemento += " alterado";
+});
+/*
+ * resultado = undefined, pois .forEach() não retorna valor, 
+ * apenas executa a função para cada elemento do vetor.
+ */ 
+console.log("resultado = " + resultadoForEach); 
+
+console.log("\n4) Elementos do vetor:");
+let resultadoMap = vet.map(function(elemento, indice) {
+  console.log(`vet[${indice}] = ${elemento}`);
+  return elemento += " alterado";
+});
+/*
+ * resultado = undefined, pois .forEach() não retorna valor, 
+ * apenas executa a função para cada elemento do vetor.
+ */ 
+console.log("resultado = " + resultadoMap);
